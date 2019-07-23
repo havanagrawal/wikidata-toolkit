4a5e502c47459f9ee0f07f3d83ea1171d59439ac
Havan <havanagrawal@gmail.com>
2019-07-23 00:24:59 -0700

    Add pydocs and functions to television models

--- model/television.py
+++ model/television.py
@@ -1,7 +1,5 @@
 """Wrapper classes for high-level concepts relating to TV series"""
 
-from functools import partial
-
 from pywikibot import ItemPage, Site
 from pywikibot.pagegenerators import WikidataSPARQLPageGenerator
 
@@ -25,6 +23,10 @@ class BaseType():
         return self._itempage.labels['en']
 
     @property
+    def title(self):
+        return self._itempage.title()
+
+    @property
     def parent(self):
         return None
 
@@ -42,40 +44,53 @@ class BaseType():
         return self._itempage.claims
 
     def __str__(self):
-        return f"{self.__class__.__name__}({self._itempage.title()} ({self.label}))"
+        return f"{self.__class__.__name__}({self.title} ({self.label}))"
 
     def __repr__(self):
         return self.__str__()
 
 class Episode(BaseType):
-    def __init__(self, itempage, repo=None):
-        super().__init__(itempage, repo)
-
+    """Encapsulates an item of instance 'television series episode'"""
     @property
     def constraints(self):
         return self._property_constraints() + self._inheritance_constraints()
 
     @property
     def parent(self):
+        """Return the Season of this Episode"""
         season_itempage = self.claims[wp.SEASON.pid][0].getTarget()
         return Season(season_itempage)
 
     @property
     def next(self):
         """Return the next episode, if any"""
+
+        # Check if it has the FOLLOWED_BY field set
         if wp.FOLLOWED_BY.pid in self.claims:
             next_episode_itempage = self.claims[wp.FOLLOWED_BY.pid][0].getTarget()
             return Episode(next_episode_itempage)
 
-        ordinal = self.ordinal_in_series
+        # Find the item that has the FOLLOWS field set to this item
+        query = generate_sparql_query({wp.FOLLOWS.pid: self.title})
+        gen = WikidataSPARQLPageGenerator(query)
+        is_followed_by = next(gen, None)
+
+        if is_followed_by is not None:
+            return Episode(is_followed_by)
+
+        # Find the item whose ordinal is one higher for this series
         if self.ordinal_in_series is not None:
             return self.next_in_series
 
+        # Find the item whose ordinal is one higher for this season
         if self.ordinal_in_season is not None:
             return self.next_in_season
 
+        return None
+
     @property
     def next_in_season(self):
+        """Return the next Episode from the same season"""
         if self.ordinal_in_season is None:
             return None
         query = f"""SELECT ?item WHERE {{
@@ -93,6 +108,7 @@ class Episode(BaseType):
 
     @property
     def next_in_series(self):
+        """Return the next Episode from the same series"""
         if self.ordinal_in_series is None:
             return None
         query = f"""SELECT ?item WHERE {{
@@ -109,12 +125,14 @@ class Episode(BaseType):
 
     @property
     def part_of_the_series(self):
+        """The ID of the series of which this episode is a part"""
         if wp.PART_OF_THE_SERIES.pid not in self.claims:
             return None
         return self.claims[wp.PART_OF_THE_SERIES.pid][0].getTarget().title()
 
     @property
     def season(self):
+        """The ID of the season of which this episode is a part"""
         if wp.SEASON.pid not in self.claims:
             return None
         return self.claims[wp.SEASON.pid][0].getTarget().title()
@@ -138,7 +156,6 @@ class Episode(BaseType):
             return None
 
         return int(series_claim.qualifiers[wp.SERIES_ORDINAL.pid][0].getTarget())
-
 
     def _property_constraints(self):
         return [has_property(prop) for prop in (
@@ -169,13 +186,28 @@ class Episode(BaseType):
         )]
 
 class Season(BaseType):
-    def __init__(self, itempage, repo=None):
-        super().__init__(itempage, repo)
-
+    """Encapsulates an item of instance 'television series season'"""
     @property
     def parent(self):
         series_itempage = self.claims[wp.PART_OF_THE_SERIES.pid][0].getTarget()
         return Series(series_itempage)
+
+    @property
+    def part_of_the_series(self):
+        """The ID of the series of which this episode is a part"""
+        if wp.PART_OF_THE_SERIES.pid not in self.claims:
+            return None
+        return self.claims[wp.PART_OF_THE_SERIES.pid][0].getTarget().title()
+
+    @property
+    def ordinal_in_series(self):
+        if not wp.PART_OF_THE_SERIES.pid in self.claims:
+            return None
+        series_claim = self.claims[wp.PART_OF_THE_SERIES.pid][0]
+        if wp.SERIES_ORDINAL.pid not in series_claim.qualifiers:
+            return None
+
+        return int(series_claim.qualifiers[wp.SERIES_ORDINAL.pid][0].getTarget())
 
     @property
     def constraints(self):
@@ -204,9 +236,7 @@ class Season(BaseType):
         )]
 
 class Series(BaseType):
-    def __init__(self, itempage, repo=None):
-        super().__init__(itempage, repo)
-
+    """Encapsulates an item of instance 'television series'"""
     @property
     def constraints(self):
         return self._property_constraints()
