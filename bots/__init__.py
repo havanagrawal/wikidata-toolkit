9719601e92dd5147c091b51909465296fcc62a5e
Havan <havanagrawal@gmail.com>
2019-08-07 00:34:32 -0700

    Add filter implementation to bots

--- bots/__init__.py
+++ bots/__init__.py
@@ -4,9 +4,9 @@ from .constraint_fixer import ConstraintCheckerBot
 from .constraint_fixer import ConstraintFixerBot
 from .constraint_fixer import AccumulatingConstraintFixerBot
 
-def getbot(generator, autofix, accumulate, always):
+def getbot(generator, autofix, accumulate, always=False, filter=""):
     if autofix:
         if accumulate:
-            return AccumulatingConstraintFixerBot(generator, always=always)
-        return ConstraintFixerBot(generator, always=always)
+            return AccumulatingConstraintFixerBot(generator, always=always, filter=filter)
+        return ConstraintFixerBot(generator, always=always, filter=filter)
     return ConstraintCheckerBot(generator, always=always)
