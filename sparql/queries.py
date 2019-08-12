b0ec81eb9857667a1abf32dc928b2f5826c7719d
Havan <havanagrawal@gmail.com>
2019-08-11 21:49:10 -0700

    Fix minor bug in missing label query

--- sparql/queries.py
+++ sparql/queries.py
@@ -26,7 +26,10 @@ def episodes(season_id):
 
 def episodes_with_titles_and_missing_labels():
     """Find English show episodes with missing labels, but with a title
-    
+
+        Missing labels are identified by checking if the label is equal to 
+        the QID
+
         Returns an iterable of (episode QID, title, series label)
     """
     query = f"""
@@ -36,7 +39,8 @@ def episodes_with_titles_and_missing_labels():
         OPTIONAL {{ ?episode wdt:{wp.TITLE.pid} ?title. }}
         OPTIONAL {{ ?episode wdt:{wp.PART_OF_THE_SERIES.pid} ?series. }}
         FILTER(BOUND(?title))
-        FILTER(REGEX(?episodeLabel, "Q\\\\d{{1,9}}"))
+        # Skip "http://www.wikidata.org/entity/" (31 characters)
+        FILTER(REGEX(?episodeLabel, SUBSTR(STR(?episode), 32)))
         SERVICE wikibase:label {{
             bd:serviceParam wikibase:language "en".
             ?episode rdfs:label ?episodeLabel.
