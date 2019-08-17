f1df060514e40393443d898ddd12fdde8fc2f02e
Havan <havanagrawal@gmail.com>
2019-08-16 23:37:49 -0700

    Update Factory to handle multi-valued P31 values

--- model/factory.py
+++ model/factory.py
@@ -3,7 +3,7 @@
 from pywikibot import Site, ItemPage
 
 from properties.wikidata_properties import INSTANCE_OF, TELEVISION_SERIES_EPISODE, TELEVISION_SERIES_SEASON, TELEVISION_SERIES
-from .television import Episode, Season, Series
+from .television import Episode, Season, Series, BaseType
 
 class Factory():
     """Factory for creating instances of the wrapper classes exposed by model"""
@@ -12,19 +12,19 @@ class Factory():
             repo = Site().data_repository()
         self.repo = repo
 
-    def get_typed_item(self, item_id):
+    def get_typed_item(self, item_id: str) -> BaseType:
         item_page = ItemPage(self.repo, item_id)
         item_page.get()
         if INSTANCE_OF.pid not in item_page.claims:
             raise ValueError(f"{item_id} has no 'instance of' property")
 
-        item_type = item_page.claims[INSTANCE_OF.pid][0]
-        instance_id = item_type.getTarget().id
-        if instance_id == TELEVISION_SERIES_EPISODE:
+        claims = item_page.claims[INSTANCE_OF.pid]
+        instance_ids = {claim.getTarget().id for claim in claims}
+        if TELEVISION_SERIES_EPISODE in instance_ids:
             return Episode(item_page, self.repo)
-        if instance_id == TELEVISION_SERIES_SEASON:
+        if TELEVISION_SERIES_SEASON in instance_ids:
             return Season(item_page, self.repo)
-        if instance_id == TELEVISION_SERIES:
+        if TELEVISION_SERIES in instance_ids:
             return Series(item_page, self.repo)
 
-        raise ValueError(f"Unsupported item of type {item_type}")
+        raise ValueError(f"Unsupported item with instance QIDs {instance_ids}")
