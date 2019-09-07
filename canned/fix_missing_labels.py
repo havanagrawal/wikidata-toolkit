902912bddf4e42a3e980731b47c6763c78342246
Havan <havanagrawal@gmail.com>
2019-09-06 17:37:23 -0700

    Improve error handling for canned query

--- canned/fix_missing_labels.py
+++ canned/fix_missing_labels.py
@@ -1,6 +1,7 @@
 """Add an English label to TV episodes which have an English title but a missing label"""
 import click
 from pywikibot import Site, ItemPage
+from pywikibot.data.api import APIError
 
 from sparql.queries import items_with_missing_labels_with_title
 
@@ -22,11 +23,19 @@ def main(dry=False):
         print(
             f"{dry_str} ( {str(item_link).ljust(40, ' ')} ) Fixing {str(item_id).ljust(9, ' ')}: {title}"
         )
-        if not dry:
-            item = ItemPage(repo, item_id)
-            item.get()
-            if len(title) < 250:
-                item.editLabels({"en": title})
+        if dry:
+            continue
+
+        # Labels have a character limit, so ignore if trying to add it will result in an error
+        if len(title) >= 250:
+            continue
+
+        item = ItemPage(repo, item_id)
+        item.get()
+        try:
+            item.editLabels({"en": title})
+        except APIError as e:
+            print(f"An error occurred while adding label for {item_id}: {e}")
 
 
 if __name__ == "__main__":
