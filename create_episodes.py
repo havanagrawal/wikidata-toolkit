6c34601be620f5ba600adfc5fad92362288b0941
Havan <havanagrawal@gmail.com>
2020-09-13 21:43:06 -0700

    Add new script to create new episodes

---
+++ create_episodes.py
@@ -0,0 +1,57 @@
+import click
+import csv
+
+from pywikibot import Claim, ItemPage, Site
+from pywikibot.data.api import Request
+
+import properties.wikidata_properties as wp
+from utils import RepoUtils
+
+from click_utils import validate_item_id
+
+def read_titles(filepath):
+    with open(filepath, 'r') as f:
+        reader = csv.reader(f)
+        return [s for s in reader]
+
+
+def create_episode(series_id, season_id, title, ordinal):
+    repoutil = RepoUtils(Site().data_repository())
+    episode = ItemPage(repoutil.repo)
+    print(f"Created a new Item: {episode.getID()}")
+    episode.editLabels({"en": title})
+
+    print(f'Setting {wp.INSTANCE_OF.pid}')
+    instance_claim = repoutil.new_claim(wp.INSTANCE_OF.pid)
+    instance_claim.setTarget(wp.TELEVISION_SERIES_EPISODE)
+    episode.addClaim(instance_claim, summary=f'Setting {wp.INSTANCE_OF.pid}')
+
+    print(f'Setting {wp.PART_OF_THE_SERIES.pid}')
+    series_claim = repoutil.new_claim(wp.PART_OF_THE_SERIES.pid)
+    series_claim.setTarget(series_id)
+    episode.addClaim(series_claim, summary=f'Setting {wp.PART_OF_THE_SERIES.pid}')
+
+    print(f'Setting {wp.SEASON.pid}')
+    season_claim = repoutil.new_claim(wp.SEASON.pid)
+    season_claim.setTarget(season_id)
+    season_ordinal = repoutil.new_claim(wp.SERIES_ORDINAL.pid)
+    season_ordinal.setTarget(ordinal)
+    season_claim.addQualifier(season_ordinal)
+    episode.addClaim(season_claim, summary=f'Setting {wp.SEASON.pid}')
+
+
+@click.command()
+@click.argument("series_id", callback=validate_item_id)
+@click.argument("season_id", callback=validate_item_id)
+@click.argument("titles_file", type=click.Path(exists=True))
+def create_episodes(series_id, season_id, titles_file):
+    titles = read_titles(titles_file)
+    for ordinal, title in titles:
+        print(title)
+        create_episode(series_id, season_id, title, ordinal)
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    create_episodes()
+
